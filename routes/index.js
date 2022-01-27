9230b355eb5825867df6fdc78f63d72b4e545fba
orpa <orpa@fresnou.org>
2022-01-27 08:54:49 -0800

    edit doesnt work

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const courseController = require('../controllers/courseController.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/courses', courseController.viewAll);
+router.get('/courses/profile/:id', courseController.viewProfile);
+router.get('/courses/edit/:id', courseController.renderEditForm);
 module.exports = router;
