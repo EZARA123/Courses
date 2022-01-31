fb441efe599f27cc9b11f9b8082bd094b5c71821
orpa <orpa@fresnou.org>
2022-01-31 08:37:06 -0800

    error

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,6 +1,10 @@
 const{Student} = require('../models')
 
 //view all
+module.exports.viewAll = async function(req,res){
+    const students = await Student.findAll();
+    res.render('student/view_all', {students});
+}
 
 //profile
 
